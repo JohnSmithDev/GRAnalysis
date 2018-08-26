e05214ac8bb0d316ddd4c7ce9249c58a10657640
John Smith (aachen) <aachen@john-smith.me>
2018-08-26 20:52:24 +0100

    Update publication_year report to use new arg handling; add -s option for spacing between years (for improved readability)

--- publication_year.py
+++ publication_year.py
@@ -8,20 +8,26 @@ Published" or "Original Publication Year" can be empty, but not both - at
 least based on the books I own.
 """
 
-from __future__ import division
+from utils.arguments import create_parser, validate_args
+from utils.export_reader import read_file
 
-from collections import defaultdict
-from functools import cmp_to_key
-import logging
-import pdb
-
-from utils.export_reader import TEST_FILE, read_file
-from utils.display import render_ratings_as_bar
 
 if __name__ == '__main__':
-    books_in_pub_date_order = sorted(read_file(TEST_FILE),
+    parser = create_parser('Output list of books ordered by publication year.',
+                           supported_args='f')
+    parser.add_argument('-s', dest='space_between_years', action='store_true',
+                        help='Output blank line between years')
+    args = parser.parse_args()
+    validate_args(args)
+
+    books_in_pub_date_order = sorted(read_file(args=args),
                                      key=lambda z: z.year or 9999)
+    prev_year = None
     for b in books_in_pub_date_order:
+        if args.space_between_years and prev_year is not None and b.year != prev_year:
+            print()
         print('%s %-5s : %s' % (b.year or '????',
                               b.rating * '*' if b.rating else '',
                               b))
+        prev_year = b.year
+
