9555f81a5889efeef958d16c0d7b34f1a4f7c34a
John Smith (aachen) <aachen@john-smith.me>
2018-08-26 17:20:01 +0100

    Make it easier to use non-standard args on particular scripts; switch TBR pile script to use new style args

--- longest_on_tbr_pile.py
+++ longest_on_tbr_pile.py
@@ -11,41 +11,53 @@ here.
 
 from functools import cmp_to_key
 
-from utils.export_reader import TEST_FILE, read_file, only_read_books, \
+from utils.arguments import create_parser, validate_args
+from utils.export_reader import read_file, only_read_books, \
     only_unread_books
-from utils.display import render_ratings_as_bar
 
-def output_report(books, min_period=None, max_books=None):
+MIN_PERIOD = 31
+
+def output_report(books, min_period=None, max_books=None, output_function=print):
     for i, bk in enumerate(books):
         if (min_period and bk.days_on_tbr_pile < min_period) or \
            (max_books and i >= max_books):
             break
-        print('%4d. %-60s : Days on TBR pile: %4d' %
-              (i+1, bk.title[:58], bk.days_on_tbr_pile))
+        output_function('%4d. %-60s - Days on TBR pile: %4d' %
+                        (i+1, bk.title[:58], bk.days_on_tbr_pile))
 
 
-if __name__ == '__main__':
-    MIN_PERIOD = 31
-    MAX_BOOKS = 50
+def report(books, label, min_period, max_books, output_function=print):
     def comparator(book_a, book_b):
         return book_b.days_on_tbr_pile - book_a.days_on_tbr_pile
 
+    output_function('== %s ==' % (label))
+    sorted_books = sorted(books, key=cmp_to_key(comparator))
+    output_report(sorted_books, min_period, max_books, output_function)
+
+
+if __name__ == '__main__':
+    parser = create_parser('Show which books have languished the longest on the TBR pile',
+                      supported_args='fl')
+    parser.add_argument('-p', dest='min_period', type=int, nargs='?',
+                        default=31, help='Minimum period to report in (in days)')
+    args = parser.parse_args()
+    validate_args(args)
+
+    if args.filters:
+        shelves_label = ' / '.join(args.filters) + ' '
+    else:
+        shelves_label = ''
 
     def only_valid_dates(bk):
+        # This is mainly for the benefit of books that were read long ago and
+        # retroactively added to GoodReads
         return bk.date_added and bk.date_read and (bk.date_read > bk.date_added)
 
-    print('== Read books ==')
-    read_books = sorted(read_file(TEST_FILE,
-                                  filter_funcs=[only_read_books, only_valid_dates]),
-                        key=cmp_to_key(comparator))
-    output_report(read_books, MIN_PERIOD, MAX_BOOKS)
-
+    report(read_file(args=args, filter_funcs=[only_read_books, only_valid_dates]),
+           'Read %sbooks' % (shelves_label), args.min_period, args.limit)
 
     print()
 
-    print('== Unread books ==')
-    unread_books = sorted(read_file(TEST_FILE,
-                                    filter_funcs=[only_unread_books]),
-                          key=cmp_to_key(comparator))
-    output_report(unread_books, MIN_PERIOD, MAX_BOOKS)
-
+    report(read_file(args=args, filter_funcs=[only_unread_books]),
+                     'Unread %sbooks' % (shelves_label),
+           args.min_period, args.limit)
--- utils/arguments.py
+++ utils/arguments.py
@@ -10,7 +10,8 @@ import pdb
 class ArgumentError(Exception):
     pass
 
-def parse_args(description, supported_args=''):
+
+def create_parser(description, supported_args):
     parser = ArgumentParser(description=description)
 
     if 'f' in supported_args:
@@ -18,12 +19,38 @@ def parse_args(description, supported_args=''):
                             action='append', default=[],
                             help='Filter only books that are on specified shelves')
 
+    if 'l' in supported_args:
+        parser.add_argument('-l', dest='limit', type=int, nargs='?',
+                            help='Limit to N results')
+
     parser.add_argument('csv_file', nargs='?',
                         default=os.environ.get('GR_CSV_FILE'),
                         help='CSV export file from GoodReads, default=GR_CSV_FILE')
-    args = parser.parse_args()
+    return parser
 
+def validate_args(args):
     if not args.csv_file:
+        # Q: Can this be specified within create_parser? This SO link implies not:
+        # https://stackoverflow.com/questions/10551117/setting-options-from-environment-variables-when-using-argparse
         raise ArgumentError('Must specify a CSV file, or set GR_CSV_FILE environment variable')
+
+
+def parse_args(description, supported_args=''):
+    """
+    Parse, validate and return the arguments.
+
+    Don't use this if you need to use non-standard/non-generic arguments,
+    instead do the following in your code:
+        parser = create_parser(...)
+        parser.add_argument(...)
+        args = parser.parse_args()
+        validate_args(args)
+        ... any additional validation
+    """
+
+
+    parser = create_parser(description, supported_args)
+    args = parser.parse_args()
+    validate_args(args)
 
     return args
