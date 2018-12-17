01127c4925a55ade3278ef1fe0b2c16eaf532288
John Smith (aachen) <github@john-smith.me>
2018-12-17 23:49:43 +0000

    New best ranked years script

---
+++ best_ranked_years.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Show average ranking of publication years, with a bar chart breaking down the rankings
+"""
+
+from __future__ import division
+
+from collections import defaultdict
+from functools import cmp_to_key
+
+from utils.arguments import parse_args
+from utils.export_reader import read_file, only_read_books
+from utils.transformers import best_ranked_report
+
+
+if __name__ == '__main__':
+    args = parse_args('Show average rating of publication years, with a bar chart breaking down the rankings',
+                      'f')
+    books = read_file(args=args, filter_funcs=[only_read_books])
+    best_ranked_report(books, 'year', ignore_single_book_groups=True)
