9280aa1511ffa4fe72946618664946a721ba33fe
John Smith (aachen) <github@john-smith.me>
2019-01-04 14:58:07 +0000

    Major reworking, in part due to adding new -I argument.

--- shelf_intersection.py
+++ shelf_intersection.py
@@ -14,31 +14,18 @@ Hint: !foo arguments are liable to confuse your shell (unless you quote/backslas
 
 """
 
+
+import logging
 import math
 import sys
 
 from utils.export_reader import read_file
 from utils.arguments import create_parser, validate_args
 
-if __name__ == '__main__':
-    parser = create_parser('List all books that are a member of one or more specified shelves, ' +
-                      'and not a memmber of any shelves prefixed with ! or ~',
-                      supported_args='efs')
-    parser.add_argument('-e', dest='enumerate_output', action='store_true',
-                        help='Enumerate (i.e. prepend a counter) each book"')
-    parser.add_argument('-i', dest='inline_separator', nargs='?',
-                        help='Output results inline, separated by supplied value"')
-    parser.add_argument('-m', dest='format', nargs='?',
-                        help='Custom output format e.g. "{title} by {author}"')
-    parser.add_argument('-p', dest='properties', action='append', default=[],
-                        help='List value of property/properties')
-    parser.add_argument('-P', dest='property_names', action='store_true',
-                        help='List all property names')
-    args = parser.parse_args()
-    validate_args(args)
+GROUP_SEPARATOR = '---'
 
+def process_books(books, args, output_function=print):
     prefix_format = '%d. '
-    books = read_file(args=args)
     if args.sort_properties:
         # TODO: support reverse sort (by prefixing prop name with ~?)
         #       Q: how would be do strings?  Have to go into cmp etc?
@@ -47,17 +34,19 @@ if __name__ == '__main__':
         b = sorted(books, key=custom_sort_key)
         books = b
         prefix_format = '%%%dd. ' % (math.ceil(math.log(len(books), 10)))
-
-
-    if args.inline_separator:
-        output_bits = []
-        def output_function(txt):
-            output_bits.append(txt)
     else:
-        output_function = print
+        def custom_sort_key(z): # Dummy function to simplify later code
+            return None
 
     prefix = ''
+    prev_sort_value = None
     for i, book in enumerate(books):
+        sort_value = custom_sort_key(book)
+        if args.inline_separator_with_breaks and sort_value != prev_sort_value \
+            and sort_value:
+            output_function(GROUP_SEPARATOR)
+            output_function('/'.join([str(z) for z in sort_value]))
+        prev_sort_value = sort_value
         if args.property_names:
             try:
                 output_function('\n'.join(book._properties()))
@@ -70,6 +59,9 @@ if __name__ == '__main__':
             output_function('%s%s' % (prefix, book.custom_format(args.format)))
         else:
             output_function('%s%s' % (prefix, book))
+
+        # args.properties doesn't work especially well with inline output -
+        # you'd be better off using a custom format
         for prop in args.properties:
             try:
                 val = getattr(book, prop)
@@ -79,5 +71,58 @@ if __name__ == '__main__':
                 val = 'Error (%s)' % (err)
             output_function('  %-20s : %s' % (prop, val))
 
-    if args.inline_separator:
-        print(args.inline_separator.join(output_bits))
+
+def output_grouped_lists(output_bits, item_separator, output_function=print):
+    def output_sublist(lst):
+        if not lst:
+            return
+        output_function('%s: %s' % (lst[0], item_separator.join(lst[1:])))
+    sublist = []
+    for item in output_bits:
+        if item == GROUP_SEPARATOR:
+            output_sublist(sublist)
+            sublist = []
+        else:
+            sublist.append(item)
+    output_sublist(sublist)
+
+
+if __name__ == '__main__':
+    parser = create_parser('List all books that are a member of one or more specified shelves, ' +
+                      'and not a memmber of any shelves prefixed with ! or ~',
+                      supported_args='efs')
+    parser.add_argument('-e', dest='enumerate_output', action='store_true',
+                        help='Enumerate (i.e. prepend a counter) each book"')
+    parser.add_argument('-i', dest='inline_separator', nargs='?',
+                        help='Output results inline, separated by supplied value"')
+    parser.add_argument('-I', dest='inline_separator_with_breaks', nargs='?',
+                        help='As -i, but do line breaks when sort value (-s changes)"')
+    parser.add_argument('-m', dest='format', nargs='?',
+                        help='Custom output format e.g. "{title} by {author}"')
+    parser.add_argument('-p', dest='properties', action='append', default=[],
+                        help='List value of property/properties')
+    parser.add_argument('-P', dest='property_names', action='store_true',
+                        help='List all property names')
+    args = parser.parse_args()
+    validate_args(args)
+
+    if args.inline_separator_with_breaks and not args.sort_properties:
+        logging.error("Must specify sort properties (-s) when using -I")
+        sys.exit(1)
+
+    if args.inline_separator or args.inline_separator_with_breaks:
+        separator = args.inline_separator or args.inline_separator_with_breaks
+        output_bits = []
+        def output_function(txt):
+            output_bits.append(txt)
+    else:
+        output_function = print
+        separator = None
+
+    process_books(read_file(args=args), args, output_function)
+
+    if separator:
+        if args.inline_separator:
+            print(separator.join(output_bits))
+        else:
+            output_grouped_lists(output_bits, separator)
