4ab64066c52f2a266929edceb8bd57ec37d548a0
John Smith (aachen) <github@john-smith.me>
2019-02-10 01:11:54 +0000

    Create draw_tsundoku_wrapper.py from the by_shelves script, as a precursor to making this generic a la the year_on_year scripts

---
+++ draw_tsundoku_wrapper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Draw a graphical representation of your to-be-read books (aka Mount Tsundoku)
+and/or your read books, colour coded by shelves.
+"""
+
+from utils.arguments import parse_args
+from utils.export_reader import read_file
+from utils.tsundoku import Tsundoku
+
+if __name__ == '__main__':
+    args = parse_args('Render a graphical representation of your to-be-read pile '
+                      'aka Mount Tsundoku, colour coded by shelves',
+                      'cdfl')
+
+    col_cfg = args.colour_cfg.select_category('shelves')
+    t = Tsundoku(col_cfg, 'user_shelves')
+    t.process(read_file(args=args))
+    t.postprocess(max_height=args.limit)
+    t.render()
+    t.output_colour_key() # Q: Or do this within .render() method?
+
