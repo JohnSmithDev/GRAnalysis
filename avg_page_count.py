9be6391575500a351c73ee1d62436e1659724fa0
John Smith (aachen) <github@john-smith.me>
2019-02-18 18:54:57 +0000

    Move calculate_average_metric function into utils/transformers, as prelude to splitting this into multiple reports

--- avg_page_count.py
+++ avg_page_count.py
@@ -11,51 +11,8 @@ from collections import defaultdict
 
 from utils.export_reader import read_file, only_read_and_rated_books
 from utils.arguments import parse_args
+from utils.transformers import calculate_average_metric
 
-def calculate_average_metric(books, keys_func, metric,
-                             include_missing_pagination=True):
-    """
-    Calculate the average value of a particular metric (currently either
-    pagination or rating), grouped by a particular key/dimension.
-    * keys_func is either the name of a property to group by, or a function
-      that takes a Book object as an argument and returns some value derived
-      from it.  Note that these can either be scalar values or iterables,
-      in the latter case all values in the iterable will be incremented
-      accordingly (e.g. a list of shelves a book is on)
-    """
-    ROGUE_KEY = '*Bad/missing %s*' % (metric)
-
-    book_count = defaultdict(int)
-    metric_count = defaultdict(int)
-
-    for book in books:
-        val = getattr(book, metric)
-
-        # TODO: I think this keys related code can be simplied using the
-        # Book.property_as_*() methods?
-        try:
-            # Try keys_func as a property of a book object
-            keys = getattr(book, keys_func)
-        except TypeError:
-            # Otherwise assume it's a function
-            keys = keys_func(book)
-        if val is not None: # Ignore books with no/undefined value
-            try:
-                book_count[keys] += 1
-                metric_count[keys] += val
-            except TypeError:
-                # Assume a list (or iterable) of keys
-                for subkey in keys:
-                    book_count[subkey] += 1
-                    metric_count[subkey] += val
-        elif include_missing_pagination:
-            book_count[ROGUE_KEY] += 1
-            metric_count[ROGUE_KEY] += 0
-    avgs = []
-    for k, numerator in metric_count.items():
-        avgs.append((k,  numerator / book_count[k], book_count[k]))
-
-    return avgs
 
 
 if __name__ == '__main__':
